30cab32ab953fdda583b7503f6e827ff86235331
karoldavid <k.zysk@zoho.com>
2017-11-23 13:52:23 +0100

    chore: use firebase as a data storage

--- App.js
+++ App.js
@@ -5,6 +5,29 @@ import store from "./src/reducers/store";
 
 export default class App extends Component {
 
+  
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    console.ignoredYellowBox = ["Setting a timer"];
+    firebase.initializeApp({
+      apiKey: "AIzaSyBOxbSD_kd9y0wAbrLXOmuqNl93rA5WA6M",
+      authDomain: "auth-ccf04.firebaseapp.com",
+      databaseURL: "https://auth-ccf04.firebaseio.com",
+      projectId: "auth-ccf04",
+      storageBucket: "",
+      messagingSenderId: "735579149305"
+    });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
   render() {
     return (
       <Provider store={store}>
