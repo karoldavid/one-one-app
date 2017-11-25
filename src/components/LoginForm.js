2298c4c986a380d67b136e3dd430159a7338d89b
karoldavid <k.zysk@zoho.com>
2017-11-25 10:47:27 +0100

    feat: show spinner while authenticating user

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -6,60 +6,70 @@ import {
   TextInput,
   TouchableOpacity,
   View,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
+  ActivityIndicator
 } from "react-native";
 
-import styles from "../utils/styles"
+import styles from "../utils/styles";
 
 class LoginForm extends Component {
+  renderLoginButton() {
+    const { email, password, loading, loginUser, navigation } = this.props;
+
+    if (this.props.loading) {
+      return <ActivityIndicator />;
+    }
+    return (
+      <TouchableOpacity
+        style={styles.submitButton}
+        onPress={() =>
+          loginUser({ email, password }, () => {
+            navigation.navigate("StudentsListView");
+          })
+        }
+      >
+        <Text style={styles.submitButtonText}> Submit </Text>
+      </TouchableOpacity>
+    );
+  }
+
   render() {
     const {
       email,
       password,
       error,
       emailChanged,
-      passwordChanged,
-      loginUser,
-      navigation
+      passwordChanged
     } = this.props;
 
     return (
       <View style={styles.container}>
-      <Text style={styles.header}>1:1 Appointments</Text>
-      <KeyboardAvoidingView style={{flex:0.8}}>
-        <TextInput
-          style={styles.input}
-          underlineColorAndroid="transparent"
-          placeholder="Email"
-          placeholderTextColor="#9a73ef"
-          autoCapitalize="none"
-          value={email}
-          onChangeText={emailChanged}
-        />
+        <Text style={styles.header}>1:1 Appointments</Text>
+        <KeyboardAvoidingView style={{ flex: 0.8 }}>
+          <TextInput
+            style={styles.input}
+            underlineColorAndroid="transparent"
+            placeholder="Email"
+            placeholderTextColor="#9a73ef"
+            autoCapitalize="none"
+            value={email}
+            onChangeText={emailChanged}
+          />
 
-        <TextInput
-          style={styles.input}
-          underlineColorAndroid="transparent"
-          placeholder="Password"
-          placeholderTextColor="#9a73ef"
-          autoCapitalize="none"
-          secureTextEntry
-          value={password}
-          onChangeText={passwordChanged}
-        />
+          <TextInput
+            style={styles.input}
+            underlineColorAndroid="transparent"
+            placeholder="Password"
+            placeholderTextColor="#9a73ef"
+            autoCapitalize="none"
+            secureTextEntry
+            value={password}
+            onChangeText={passwordChanged}
+          />
 
-        <TouchableOpacity
-          style={styles.submitButton}
-          onPress={() =>
-            loginUser({ email, password }, () => {
-              navigation.navigate("StudentsListView");
-            })
-          }
-        >
-          <Text style={styles.submitButtonText}> Submit </Text>
-        </TouchableOpacity>
-        <Text>{error}</Text>
-      </KeyboardAvoidingView>
+          <View>{this.renderLoginButton()}</View>
+          <Text>{error}</Text>
+        </KeyboardAvoidingView>
       </View>
     );
   }
