4ad9475d78285cf6033f92660ed55b1424ed6037
karoldavid <k.zysk@zoho.com>
2017-11-30 17:34:51 +0100

    feat: show student data in student view

--- src/components/StudentView.js
+++ src/components/StudentView.js
@@ -1,10 +1,51 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { connect } from "react-redux";
+import { Image, Text, View, StyleSheet } from "react-native";
+import { blue, white } from "../utils/colors";
 
 class StudentView extends Component {
 	render() {
-		return <Text>StudentView</Text>;
+		const { image, name, email, program } = this.props.student;
+		console.log(this.props.student);
+		return (
+			<View style={styles.container}>
+				<Image source={{ uri: image }} style={styles.photo} />
+				<View style={{ flexDirection: "column" }}>
+					<Text style={styles.text}>{`${name.first} ${
+						name.last
+					}`}</Text>
+					<Text style={styles.text}>{email}</Text>
+					<Text style={styles.text}>{program}</Text>
+				</View>
+			</View>
+		);
 	}
 }
 
-export default StudentView;
+const mapStateToProps = state => {
+	return {
+		student: state.student
+	};
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 12,
+		flexDirection: "row",
+		alignItems: "flex-start",
+		backgroundColor: white
+	},
+	text: {
+		marginLeft: 12,
+		paddingBottom: 10,
+		fontSize: 16,
+		color: blue
+	},
+	photo: {
+		height: 150,
+		width: 100
+	}
+});
+
+export default connect(mapStateToProps)(StudentView);
