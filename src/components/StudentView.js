90fc1f7d026e042ba2fc7360b4dfe96b02d8f0e3
karoldavid <k.zysk@zoho.com>
2017-12-01 12:11:37 +0100

    fix: change name properties

--- src/components/StudentView.js
+++ src/components/StudentView.js
@@ -5,14 +5,13 @@ import { blue, white } from "../utils/colors";
 
 class StudentView extends Component {
 	render() {
-		const { image, name, email, program } = this.props.student;
-		console.log(this.props.student);
+		const { firstName, lastName, email, program, image } = this.props.student;
 		return (
 			<View style={styles.container}>
 				<Image source={{ uri: image }} style={styles.photo} />
 				<View style={{ flexDirection: "column" }}>
-					<Text style={styles.text}>{`${name.first} ${
-						name.last
+					<Text style={styles.text}>{`${firstName} ${
+						lastName
 					}`}</Text>
 					<Text style={styles.text}>{email}</Text>
 					<Text style={styles.text}>{program}</Text>
