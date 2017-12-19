813403aa144e85c1a95811a0aca038848d91c2d0
karoldavid <k.zysk@zoho.com>
2017-12-19 21:48:45 +0100

    feat: show all appointments in agenda view

--- src/components/AgendaScreen.js
+++ src/components/AgendaScreen.js
@@ -1,21 +1,24 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Text, View, StyleSheet } from "react-native";
 import { Agenda } from "react-native-calendars";
+import moment from "moment";
 
-// @TODO: use redux to update calendar 
-// @TODO: 
+// @TODO: use redux to update calendar
+// @TODO:
 
-export default class AgendaScreen extends Component {
+class AgendaScreen extends Component {
   state = {
     items: {}
   };
 
   render() {
+
     return (
       <Agenda
         items={this.state.items}
         loadItemsForMonth={this.loadItems.bind(this)}
-        selected={Date()}
+        selected={"2016-12-10T07:30:00+01:00"}
         renderItem={this.renderItem.bind(this)}
         renderEmptyDate={this.renderEmptyDate.bind(this)}
         rowHasChanged={this.rowHasChanged.bind(this)}
@@ -37,37 +40,39 @@ export default class AgendaScreen extends Component {
   }
 
   loadItems(day) {
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = this.timeToString(time);
-        if (!this.state.items[strTime]) {
-          this.state.items[strTime] = [];
-          const numItems = Math.floor(Math.random() * 5);
-          for (let j = 0; j < numItems; j++) {
-            this.state.items[strTime].push({
-              name: "Item for " + strTime,
-              height: Math.max(50, Math.floor(Math.random() * 150))
-            });
-          }
-        }
+    const newItems = {};
+
+    this.props.appointments.map(appointment => {
+      const { timeDateUtc, description, project } = appointment;
+      const strDate = timeDateUtc.split("T")[0];
+      const strTime = moment(timeDateUtc).local().format('HH:mm')
+
+      if (!this.state.items[strDate]) {
+        this.state.items[strDate] = [];
       }
-      //console.log(this.state.items);
-      const newItems = {};
-      Object.keys(this.state.items).forEach(key => {
-        newItems[key] = this.state.items[key];
+
+      this.state.items[strDate].push({
+        time: strTime,
+        project: project,
+        description: description
       });
-      this.setState({
-        items: newItems
-      });
-    }, 1000);
-    // console.log(`Load Items for ${day.year}-${day.month}`);
+    });
+    
+    Object.keys(this.state.items).forEach(key => {
+      newItems[key] = this.state.items[key];
+    });
+
+    this.setState({
+      items: newItems
+    });
   }
 
   renderItem(item) {
     return (
       <View style={[styles.item, { height: item.height }]}>
-        <Text>{item.name}</Text>
+        <Text style={{ color: "blue"}}>{item.time}</Text>
+        <Text>{item.project}</Text>
+        <Text>{item.description}</Text>
       </View>
     );
   }
@@ -105,3 +110,11 @@ const styles = StyleSheet.create({
     paddingTop: 30
   }
 });
+
+const mapStateToProps = ({ appointmentList }) => {
+  return {
+    appointments: appointmentList.appointments
+  };
+};
+
+export default connect(mapStateToProps, {})(AgendaScreen);
